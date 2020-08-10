e5e13c4b702e8effe1a9639633bcd7835d0a7669
DEv-NE786 <69320234+DEv-NE786@users.noreply.github.com>
2020-08-10 23:48:11 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,4 @@
-const { play } = require("../include/play.js");
+const { play } = require("../include/play");
 const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID } = require("../config.json");
 const ytdl = require("ytdl-core");
 const YouTubeAPI = require("simple-youtube-api");
