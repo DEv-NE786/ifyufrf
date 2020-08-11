31eaac313b434746acbdddf6aa014c3a51e27ce9
DEv-NE786 <69320234+DEv-NE786@users.noreply.github.com>
2020-08-11 14:23:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1366,11 +1366,5 @@ client.music.start(client, {
   // Set the api key used for YouTube.
   // This is required to run the bot.
   youtubeKey: "AIzaSyCgLBLM8Uv1xBFhnvKJ4g7paf8JSUMQVFg"
-  cooldown: {
-    // This disables the cooldown. Not recommended.
-    enabled: true
-    }
-    let prefix = "@"
-	
 });
 client.login(process.env.TOKEN)//توكن بوتك
