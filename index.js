2b9bfa0824d5d53b4ec69bb0a93efed2a8bc39b1
DEv-NE786 <69320234+DEv-NE786@users.noreply.github.com>
2020-08-11 04:03:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1359,92 +1359,18 @@ let content = msg.content.split('').map(letter => emojiChars[letter]).join(' ');
 return msg.channel.send(`${content ? content : 'Failed to emojify'}`);
   }
 });
-const Enmap = require('enmap');
-const EnmapSQLite = require('enmap-sqlite');
-const { token, defaultSettings } = require('./config.json');
+client.music = require("discord.js-musicbot-addon");
 
-const files = fs.readdirSync('./commands');
-const events = fs.readdirSync('./events');
-if (!files.length) throw Error('No command files found!');
-if (!events.length) throw Error('No event files found!');
-const jsfiles = files.filter(c => c.split('.').pop() === 'js');
-const jsevents = events.filter(c => c.split('.').pop() === 'js');
-
-class Client extends Discord.Client {
-  constructor() {
-    super();
-
-    this.commands = new Discord.Collection();
-
-    this.aliases = new Discord.Collection();
-
-    this.playlists = new Discord.Collection();
-
-    this.embed = require('./util/Embeds');
-
-    this.settings = new Enmap({ provider: new EnmapSQLite({ name: 'settings' }) });
-
-
-  }
-
-  setGuildSettings(id, obj) {
-    return this.settings.set(id, obj);
-  }
-
-  addGuildSettings(name, val) {
-    this.guilds.forEach(c => {
-      const id = c.id;
-      const current = this.settings.get(id);
-      current[name] = val;
-      this.settings.set(id, current);
-    });
-  }
-
-  removeGuildSettings(id, name) {
-    const current = this.settings.get(id);
-    delete current[name];
-    this.settings.set(id, current);
-  }
-
-  setDefaultGuildSettings(id) {
-    this.settings.set(id, defaultSettings);
-  }
-
-  setPropSettings(id, name, val) {
-    const current = this.settings.get(id);
-    current[name] = val;
-    this.settings.set(id, current);
-  }
-
-  log(message) {
-    console.log(`[${new Date().toLocaleString()}] > ${message}`);
-  }
-}
-
-const client = new Client();
-
-for (let i = 0; i < jsfiles.length; i++) {
-  if (!jsfiles.length) throw Error('No javascript command files found!');
-  const file = require(`./commands/${jsfiles[i]}`);
-  const command = new file(client);
-  if (typeof command.run !== 'function') throw Error(`No run function found in ${jsfiles[i]}`);
-  client.commands.set(command.name, command);
-  client.log(`Command loaded: ${command.name} `);
-  if (command && command.aliases && command.aliases.constructor.name === 'Array') {
-    for (let i = 0; i < command.aliases.length; i++) {
-      client.aliases.set(command.aliases[i], command);
-    }
-  }
-}
-
-for (let i = 0; i < jsevents.length; i++) {
-  if (!jsevents.length) throw Error('No javascript event files found!');
-  const file = require(`./events/${jsevents[i]}`);
-  const event = new file(client, file);
-  client.log(`Event loaded: ${event.name}`);
-  if (typeof event.run !== 'function') throw Error(`No run function found in ${jsevents[i]}`);
-  client.on(jsevents[i].split('.')[0], (...args) => event.run(...args));
-}
-client.login(process.env.TOKEN)//
-process.on('unhandledRejection', err => client.log(err));
+// Now we start the music module.
+client.music.start(client, {
+  // Set the api key used for YouTube.
+  // This is required to run the bot.
+  youtubeKey: "AIzaSyCgLBLM8Uv1xBFhnvKJ4g7paf8JSUMQVFg"
+  ownerOverMember: true,
+  ownerID: "706161534233083964",
+  cooldown: {
+    // This disables the cooldown. Not recommended.
+    enabled: true
+    let prefix = "@"
+});
 client.login(process.env.TOKEN)//توكن بوتك
